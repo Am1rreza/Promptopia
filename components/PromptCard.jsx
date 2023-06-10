d0d12064768e8484fdfd4b5c77c76227b7ecc416
Amirreza <amirreza.m13825@gmail.com>
2023-06-10 18:34:47 +0330

    profile page and component developed

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -10,7 +10,6 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
   const handleCopy = () => {
     setCopied(post.prompt);
-    console.log(post);
     navigator.clipboard.writeText(post.prompt);
 
     setTimeout(() => setCopied(""), 3000);
